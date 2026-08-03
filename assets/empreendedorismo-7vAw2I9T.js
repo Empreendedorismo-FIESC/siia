f5a7bf73af6c740836617e31546677094d1b570b
MAHER BEN DAVID RAMOS DAOUD <maher-daoud@sc.senai.br>
2026-08-03 13:39:43 -0300

    fix: url api

---
+++ assets/empreendedorismo-7vAw2I9T.js
@@ -0,0 +1,1 @@
+import{_ as e,P as r}from"./tailwind-D179n-KL.js";const c={};function n(o,s){return" Teste "}const t=e(c,[["render",n]]);r(t).mount("#app");
--- assets/empreendedorismo-DXBWHoV4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,P as r}from"./tailwind-C6LnEpsk.js";const c={};function n(o,s){return" Teste "}const t=e(c,[["render",n]]);r(t).mount("#app");
